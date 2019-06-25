9372ccd41046715acbed4c03f17f4b6a716a95a5
Maximiliano Ruiz <maximiliano.ruiz@bci.cl>
2019-06-25 19:09:50 -0400

    Auto Layout calculator mvp1

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Maximiliano Ruiz on 6/16/19.
+//  Copyright © 2019 Maximiliano Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
